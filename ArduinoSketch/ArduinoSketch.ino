4a519f54c1c1f7c2ba7d40605f68c22c312d0b1e
Israel Lins Albuquerque <israel@proabakus.com.br>
2021-11-19 18:03:53 -0300

    commit inicial

---
+++ ArduinoSketch/ArduinoSketch.ino
@@ -0,0 +1,39 @@
+#include <LinkedList.h>
+
+String g_ssid = "";
+
+struct Comando {
+  String nome;
+  LinkedList<String> args
+};
+
+
+void inicializarWifi() {
+
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  inicializarWifi();
+}
+
+void verificarConexaoWifi() {
+
+}
+
+void enviarBeacon() {
+
+}
+
+void processarComandosRecebidos(Comando a_comando) {
+  (void)(a_comando);
+}
+
+void receberComandos() {
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
